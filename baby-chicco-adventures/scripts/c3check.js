a9edf76dc3e1dbc62082741a9fb13e9ac4f43af5
pixel1828 <nathan.dansand@gmail.com>
2026-06-24 17:11:02 -0500

    Add c3check.js with error XML response

---
+++ baby-chicco-adventures/scripts/c3check.js
@@ -0,0 +1,2 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<Error><Code>NoSuchKey</Code><Message>The specified key does not exist.</Message><Key>baby-chicco-adventures/10/scripts/c3check</Key><RequestId>K29QTP5S3DV6GBNW</RequestId><HostId>ZcXFNhjOGl2sDboukxtyovCTRyPRZ8/cAN0IIZecDLPNZYN5PwDjavotgt0ENr8mcN5XbB+h/73pwpb3yIQzWiaMpj9iSfIp</HostId></Error>
