3a694c17aaf6a0baad0d3bdc63a7a1947a8c801f
pixel1828 <nathan.dansand@gmail.com>
2026-09-24 14:07:14 -0500

    resurrect baby chicco by supporting our enemies

--- baby-chicco-adventures/scripts/project/main.js
+++ baby-chicco-adventures/scripts/project/main.js
@@ -1,1 +1,5 @@
-
+const sdkElem = document.createElement("script");
+sdkElem.type = "text/javascript";
+sdkElem.src = "https://sdk.crazygames.com/Construct3CrazySDK-v2.js";
+document.body.appendChild(sdkElem);
+//dammit i hate crazygames... their my worst enemy
